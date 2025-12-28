0aad26fc998ae17d33dc051d61ca7a03785769bf
jason03300305 <124381285+jason03300305@users.noreply.github.com>
2025-12-28 15:49:46 +0800

    upload file

---
+++ index.js
@@ -0,0 +1,26 @@
+let homeScore = document.getElementById("home-score") 
+let guestScore = document.getElementById("guest-score")
+
+function homeBtn1(){
+    homeScore.textContent = Number(homeScore.textContent) + 1
+}
+function homeBtn2(){
+    homeScore.textContent = Number(homeScore.textContent) + 2
+}
+function homeBtn3(){
+    homeScore.textContent = Number(homeScore.textContent) + 3
+}
+
+function guestBtn1(){
+    guestScore.textContent = Number(guestScore.textContent) + 1
+}
+function guestBtn2(){
+    guestScore.textContent = Number(guestScore.textContent) + 2
+}
+function guestBtn3(){
+    guestScore.textContent = Number(guestScore.textContent) + 3
+}
+function reset(){
+    guestScore.textContent = 0
+    homeScore.textContent = 0
+}
